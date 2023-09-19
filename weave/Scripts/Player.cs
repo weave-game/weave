55fda3b8efc587e1db4241bcdc11bf015671b92d
theowiik <theo.le.wiik@gmail.com>
2023-09-19 14:27:53 +0200

    Extract player to its own scene

--- weave/Scripts/Player.cs
+++ weave/Scripts/Player.cs
@@ -8,34 +8,27 @@ public partial class Player : RigidBody2D
     private const int MovementSpeed = 100;
     private PackedScene _bombScene = GD.Load<PackedScene>("res://Objects/Bomb.tscn");
 
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-    }
-
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(double delta)
-    {
-        ApplyTorqueImpulse(1000 * (float)delta);
-    }
-
     public override void _PhysicsProcess(double delta)
     {
-        var input = GetInput();
-        Translate(input * MovementSpeed * (float)delta);
+        Move(delta);
 
         if (Input.IsActionJustPressed("shoot"))
             Shoot();
     }
 
+    private void Move(double delta)
+    {
+        var input = GetInput();
+        Translate(input * MovementSpeed * (float)delta);
+    }
+
     private void Shoot()
     {
-        RigidBody2D bomb = _bombScene.Instantiate<RigidBody2D>();
-        // AddChild(bomb); // <- no
+        var bomb = _bombScene.Instantiate<RigidBody2D>();
         EmitSignal(SignalName.PlayerShotBullet, bomb, GlobalPosition);
     }
 
-    private Vector2 GetInput()
+    private static Vector2 GetInput()
     {
         var input = new Vector2();
 
