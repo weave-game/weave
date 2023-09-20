345ffea60dd598833a8d9e483fad77f535744304
theowiik <42991003+theowiik@users.noreply.github.com>
2023-09-20 22:21:07 +0200

    Animate a crown

--- scoreboard/scoreboard-frontend/src/App.vue
+++ scoreboard/scoreboard-frontend/src/App.vue
@@ -3,9 +3,10 @@ import ListView from './components/ListView.vue'
 </script>
 
 <template>
-  <h1>Scoreboard</h1>
-
-  <ListView />
+  <div class="container mx-auto px-5 pt-16">
+    <h1 class="text-8xl font-bold text-white catcher-font">Scoreboard</h1>
+    <ListView />
+  </div>
 </template>
 
 <style scoped></style>
--- scoreboard/scoreboard-frontend/src/components/ListView.vue
+++ scoreboard/scoreboard-frontend/src/components/ListView.vue
@@ -57,15 +57,43 @@ onUnmounted(() => {
 </script>
 
 <template>
-  <div class="card">
+  <div class="text-neutral-500 font-bold">
     <p>Fetching scores in: {{ countdown }} seconds</p>
   </div>
 
-  <ul>
-    <li v-for="score in scores" :key="score.name">
-      {{ score.name }}: {{ score.score }}
-    </li>
-  </ul>
+  <div v-for="(score, index) in scores" :key="score.name" class="mt-16">
+    <div class="flex flex-row">
+      <div>
+        <h2 class="text-7xl font-black text-white">{{ index + 1 }}. {{ score.name }}</h2>
+        <h3 class="text-5xl my-mono font-bold text-neutral-500">{{ score.score }}</h3>
+      </div>
+
+      <img v-if="index === 0"
+        src="https://static.vecteezy.com/system/resources/thumbnails/010/331/776/small/3d-rendering-gold-crown-with-three-blue-diamonds-isolated-png.png"
+        alt="crown" class="crown ml-7">
+    </div>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+@keyframes float {
+  0% {
+    transform: translatey(0px) rotate(0deg);
+  }
+
+  50% {
+    transform: translatey(-20px) rotate(-10deg);
+  }
+
+  100% {
+    transform: translatey(0px) rotate(0deg);
+  }
+}
+
+.crown {
+  transform: translatey(0px);
+  animation: float 6s ease-in-out infinite;
+  width: 80px;
+  height: 80px;
+}
+</style>
