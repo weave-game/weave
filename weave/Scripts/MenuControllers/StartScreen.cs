e403b482e624e6d23840a05d2c4d909daf415500
Elias Carlson <carlson.elias@gmail.com>
2023-09-21 15:57:28 +0200

    Added barebones start menu

--- weave/Scripts/MenuControllers/StartScreen.cs
+++ weave/Scripts/MenuControllers/StartScreen.cs
@@ -7,17 +7,25 @@ public partial class StartScreen : Node2D
 {
     private PackedScene _gameScene = GD.Load<PackedScene>("res://Scenes/Main.tscn");
 
-    [GetNode("CanvasLayer/CenterContainer/VBoxContainer/Button")]
+    [GetNode("CanvasLayer/CenterContainer/VBoxContainer/StartButton")]
     private Button _startButton;
 
+    [GetNode("CanvasLayer/CenterContainer/VBoxContainer/QuitButton")]
+    private Button _quitButton;
     public override void _Ready()
     {
         this.GetNodes();
         _startButton.Pressed += OnStartButtonPressed;
+        _quitButton.Pressed += OnQuitButtonPressed;
     }
 
     private void OnStartButtonPressed()
     {
         GetTree().ChangeSceneToPacked(_gameScene);
+    }
+
+    private void OnQuitButtonPressed()
+    {
+        GetTree().Quit();
     }
 }
