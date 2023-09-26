2297a8d1d5d420e9a8287d2998b635d8a2cf6660
Erik Wessman <erikwessman10@gmail.com>
2023-09-26 15:27:08 +0200

    Cont

--- weave/Scripts/FlyingLight.cs
+++ weave/Scripts/FlyingLight.cs
@@ -8,24 +8,49 @@ public partial class FlyingLight : Area2D
     private PathFollow2D pathFollow;
     private float Speed;
     private float GoalSpeed;
-    private int _step;
-
     private float lastSpeed;
     private float goalSpeed;
+    private const int NrPoints = 10;
+    private Vector2[] points;
+    private float distBetweenPoints = 1;
 
     public override void _Ready()
     {
         pathFollow = GetParent<PathFollow2D>();
+
+        points = new Vector2[NrPoints];
+        for (var i = 0; i < NrPoints; i++)
+        {
+            points[i] = new Vector2(Position.X, Position.Y);
+        }
     }
 
     public override void _Process(double delta)
     {
-        if (MathF.Abs(Speed - GoalSpeed) < (float) 10e-5)
+        if (MathF.Abs(Speed - GoalSpeed) < (float)10e-5)
         {
             GoalSpeed = GD.Randf() * 10;
         }
+
+        var lastPoint = GlobalPosition;
+
+        for (var i = 0; i < NrPoints; i++)
+        {
+            if (points[i].DistanceTo(lastPoint) >= distBetweenPoints)
+            {
+                points[i].X = GlobalPosition.X;
+                points[i].Y = GlobalPosition.Y;
+            }
+            else if (i > 0)
+            {
+                points[i].X = points[i-1].X;
+                points[i].Y = points[i-1].Y;
+            }
+            
+            GD.Print($"Point nr {i+1} is at position {points[i]}");
+        }
+
         Speed = Mathf.Lerp(Speed, GoalSpeed, 0.3f);
         pathFollow.Progress += Speed;
-        _step++;
     }
 }
