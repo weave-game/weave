ad32927392998cbf231808064dba58009feadd43
theowiik <theo.le.wiik@gmail.com>
2023-09-29 15:37:17 +0200

    Add gamepad controller

---
+++ weave/Scripts/InputHandlers/GamepadController.cs
@@ -0,0 +1,14 @@
+namespace weave.InputHandlers;
+
+public sealed class GamepadController : IController
+{
+    public bool IsTurningLeft()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public bool IsTurningRight()
+    {
+        throw new System.NotImplementedException();
+    }
+}
--- weave/Scripts/InputHandlers/KeyboardController.cs
+++ weave/Scripts/InputHandlers/KeyboardController.cs
@@ -2,7 +2,7 @@ using Godot;
 
 namespace weave.InputHandlers;
 
-public class KeyboardController : IController
+public sealed class KeyboardController : IController
 {
     private readonly Key _left;
     private readonly Key _right;
