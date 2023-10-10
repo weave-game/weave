6f98b123e04a1e4edc6f4d21036fb1f70f13a4d9
theowiik <42991003+theowiik@users.noreply.github.com>
2023-10-10 21:17:53 +0200

    Pitch on lose

--- weave/Scripts/Main.cs
+++ weave/Scripts/Main.cs
@@ -23,6 +23,9 @@ public partial class Main : Node2D
     private const int PlayerStartDelay = 2;
     private readonly ISet<Player> _players = new HashSet<Player>();
 
+    [GetNode("AudioStreamPlayer2D")]
+    private AudioStreamPlayer2D _audioStreamPlayer2D;
+
     [GetNode("CountdownLayer/CenterContainer/CountdownLabel")]
     private CountdownLabel _countdownLabel;
 
@@ -163,6 +166,8 @@ public partial class Main : Node2D
         _gameOverOverlay.Visible = true;
         _gameOverOverlay.FocusRetryButton();
         ProcessMode = ProcessModeEnum.Disabled;
+
+        _audioStreamPlayer2D.PitchScale = 0.5f;
     }
 
     private ISet<SegmentShape2D> GetAllSegments()
