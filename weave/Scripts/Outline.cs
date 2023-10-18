46d1d5d4a466b3a7335032ec84e881e7d1d2bf19
SimplemathicDev <v.levente1999@gmail.com>
2023-10-18 22:46:49 +0200

    Format

--- weave/Scripts/Outline.cs
+++ weave/Scripts/Outline.cs
@@ -8,21 +8,19 @@ public partial class Outline : Line2D
     public override void _Ready()
     {
         Width = WeaveConstants.LineWidth;
-        
+
         var width = GetViewportRect().Size.X;
         var height = GetViewportRect().Size.Y;
 
         ClearPoints();
         AddPoint(new(-WeaveConstants.LineWidth / 2, -WeaveConstants.LineWidth / 2));
-        AddPoint(new(width + WeaveConstants.LineWidth / 2, -WeaveConstants.LineWidth / 2));
-        AddPoint(new(width + WeaveConstants.LineWidth / 2, height + WeaveConstants.LineWidth / 2));
-        AddPoint(new(-WeaveConstants.LineWidth / 2, height + WeaveConstants.LineWidth / 2));
+        AddPoint(new(width + (WeaveConstants.LineWidth / 2), -WeaveConstants.LineWidth / 2));
+        AddPoint(new(width + (WeaveConstants.LineWidth / 2), height + (WeaveConstants.LineWidth / 2)));
+        AddPoint(new(-WeaveConstants.LineWidth / 2, height + (WeaveConstants.LineWidth / 2)));
         AddPoint(Points[0] + new Vector2(0, -WeaveConstants.LineWidth / 2));
-
     }
 
     public override void _Process(double delta)
     {
-        
     }
 }
