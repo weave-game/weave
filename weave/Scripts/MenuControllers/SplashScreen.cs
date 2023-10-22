5946f66b4bcb8fb424cf79542c826a24031f41e1
theowiik <42991003+theowiik@users.noreply.github.com>
2023-10-22 15:51:33 +0200

    Disable key presses in the splash screen for the first second

--- weave/Scripts/MenuControllers/SplashScreen.cs
+++ weave/Scripts/MenuControllers/SplashScreen.cs
@@ -20,8 +20,22 @@ public partial class SplashScreen : Node2D
     [GetNode("AudioStreamPlayer")]
     private AudioStreamPlayer _music;
 
+    /// <summary>
+    ///     Whether the scene is fading out.
+    /// </summary>
     private bool _isFading;
+
+    /// <summary>
+    ///    Whether the player has pressed a key at least once, this shows the skip label and allows the player to skip the scene.
+    /// </summary>
     private bool _hasPressed;
+
+    /// <summary>
+    ///     Whether the player is press the "skip" key.
+    ///     Is used to prevent skipping in the first frames since people are spamming keys in the lobby.
+    /// </summary>
+    private bool _allowInputs;
+
     private const float MusicFinalPitch = 0.7f;
     private readonly Color _fadeColor = new("1e1e1eff");
 
@@ -30,6 +44,7 @@ public partial class SplashScreen : Node2D
         this.GetNodes();
         _skipLabel.Visible = false;
 
+        AddChild(TimerFactory.StartedOneShot(1, () => _allowInputs = true));
         AddChild(TimerFactory.StartedOneShot(7, () => _isFading = true));
     }
 
@@ -51,6 +66,8 @@ public partial class SplashScreen : Node2D
 
     public override void _Input(InputEvent @event)
     {
+        if (!_allowInputs) return;
+
         if (@event is not InputEventKey { Pressed: true })
         {
             return;
