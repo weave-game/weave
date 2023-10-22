ac9f5f5d3d6ed30bc655bb036bda3b9073b6ed6b
theowiik <42991003+theowiik@users.noreply.github.com>
2023-10-22 20:49:28 +0200

    Organize list view

--- scoreboard/frontend/src/components/ListView.vue
+++ scoreboard/frontend/src/components/ListView.vue
@@ -1,83 +1,88 @@
 <script setup lang="ts">
-import { ref, onMounted, onUnmounted } from 'vue'
+import { ref, onMounted, onUnmounted } from "vue";
 
 type Score = {
   id: string;
-  name: string
-  points: number
-}
+  name: string;
+  points: number;
+};
 
 type ScoresDTO = {
-  scores: Score[],
-  timestamp: string,
+  scores: Score[];
+  timestamp: string;
   error: {
-    message: string
-  }
-}
+    message: string;
+  };
+};
 
-const scores = ref([] as Score[])
-const fetchIntervalSeconds = 5
-const scoresToDisplay = 10
-const countdown = ref(fetchIntervalSeconds)
-const hiddenScores = ref<string[]>(JSON.parse(localStorage.getItem('hiddenScores') || '[]'))
+const scores = ref([] as Score[]);
+const fetchIntervalSeconds = 5;
+const scoresToDisplay = 10;
+const countdown = ref(fetchIntervalSeconds);
+const hiddenScores = ref<string[]>(
+  JSON.parse(localStorage.getItem("hiddenScores") || "[]")
+);
 
 // Fetch scores from the server
 const fetchScores = async () => {
   try {
-    const response = await fetch('http://localhost:3000/scores')
+    const response = await fetch("http://localhost:3000/scores");
 
     if (!response.ok) {
-      throw new Error('Failed to fetch scores')
+      throw new Error("Failed to fetch scores");
     }
 
-    const data: ScoresDTO = await response.json()
+    const data: ScoresDTO = await response.json();
     scores.value = filter(data.scores);
   } catch (error) {
-    console.error('Error fetching scores:', error)
+    console.error("Error fetching scores:", error);
   }
-}
+};
 
 const toggleScoreVisibility = (name: string) => {
-  if (window.confirm('Are you sure you want to hide this score?')) {
-    hiddenScores.value.push(name)
-    localStorage.setItem('hiddenScores', JSON.stringify(hiddenScores.value))
+  if (window.confirm("Are you sure you want to hide this score?")) {
+    hiddenScores.value.push(name);
+    localStorage.setItem("hiddenScores", JSON.stringify(hiddenScores.value));
   }
-}
+};
 
 const filter = (scores: Score[]) => {
-  const visibleScores = scores.filter(score => !hiddenScores.value.includes(score.id))
-  const sorted = visibleScores.sort((a, b) => b.points - a.points).slice(0, scoresToDisplay)
+  const visibleScores = scores.filter(
+    (score) => !hiddenScores.value.includes(score.id)
+  );
+  const sorted = visibleScores
+    .sort((a, b) => b.points - a.points)
+    .slice(0, scoresToDisplay);
 
   if (scores.length > scoresToDisplay) {
     const fakeScore: Score = {
-      name: '...',
-    } as Score
+      name: "...",
+    } as Score;
 
-    sorted.splice(sorted.length - 1, 0, fakeScore)
+    sorted.splice(sorted.length - 1, 0, fakeScore);
   }
 
   return sorted;
-}
+};
 
-let fetchInterval: number
-let countdownInterval: number
+let fetchInterval: number;
+let countdownInterval: number;
 
 onMounted(() => {
-  fetchScores()
-  fetchInterval = setInterval(fetchScores, fetchIntervalSeconds * 1000)
+  fetchScores();
+  fetchInterval = setInterval(fetchScores, fetchIntervalSeconds * 1000);
   countdownInterval = setInterval(() => {
-    countdown.value--
+    countdown.value--;
     if (countdown.value <= 0) {
-      countdown.value = fetchIntervalSeconds
+      countdown.value = fetchIntervalSeconds;
     }
-  }, 1000)
-})
+  }, 1000);
+});
 
 onUnmounted(() => {
-  clearInterval(fetchInterval)
-  clearInterval(countdownInterval)
-})
-
+  clearInterval(fetchInterval);
+  clearInterval(countdownInterval);
+});
 </script>
 
 <template>
@@ -87,22 +92,36 @@ onUnmounted(() => {
 
   <table class="text-white w-full text-3xl font-black my-16 border-solid border-2 border-neutral-800">
     <tbody>
-      <tr v-for="(score, index) in scores" :key="score.name" class="pt-32" style="height: 80px"
-        :class="{ 'bg-neutral-800': index % 2 === 0, 'bg-neutral-900': index % 2 === 1 }">
+      <tr v-for="(score, index) in scores" :key="score.name" class="pt-32" style="height: 80px" :class="{
+        'bg-neutral-800': index % 2 === 0,
+        'bg-neutral-900': index % 2 === 1,
+      }">
+        <!-- Position -->
         <td class="pl-8">{{ index + 1 }}.</td>
-        <td>{{ score.name }}</td>
+
+        <!-- Team name -->
         <td>
-          <div class="flex items-center">
-            <span class="my-mono">
-              {{ score.points.toLocaleString('sv-SE') }}
-            </span>
+          {{ score.name }}
+        </td>
 
-            <img v-if="index === 0" src="../assets/img/crown.png" alt="crown" class="crown ml-7">
+        <!-- Score -->
+        <td class="text-end">
+          <span class="my-mono">
+            {{ score.points.toLocaleString("sv-SE") }}
+          </span>
+        </td>
 
-            <img v-if="index + 1 === scores.length" src="../assets/img/l.gif" alt="crown" class="crown ml-7">
+        <!-- Possible images -->
+        <td>
+          <img v-if="index === 0" src="../assets/img/crown.png" alt="crown" class="crown ml-7" />
+          <img v-if="index + 1 === scores.length" src="../assets/img/l.gif" alt="crown" class="crown ml-7" />
+        </td>
 
-            <button @click="toggleScoreVisibility(score.id)" class="ml-3">--- Hide</button>
-          </div>
+        <!-- Hide button -->
+        <td>
+          <button @click="toggleScoreVisibility(score.id)" class="ml-3">
+            X
+          </button>
         </td>
       </tr>
     </tbody>
