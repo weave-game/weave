9f7e6a5797a07582f648b96cc28d01d1ca576a28
theowiik <42991003+theowiik@users.noreply.github.com>
2023-10-22 17:10:08 +0200

    Add a simple credits view

---
+++ weave/Scripts/MenuControllers/Credits.cs
@@ -0,0 +1,16 @@
+using Godot;
+
+namespace Weave.MenuControllers;
+
+public partial class Credits : CanvasLayer
+{
+    public override void _UnhandledInput(InputEvent @event)
+    {
+        if (@event is InputEventMouseMotion)
+        {
+            return;
+        }
+
+        Hide();
+    }
+}
